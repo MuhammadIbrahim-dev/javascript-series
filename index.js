5dba52c2bda96b4dedfcbe570dd76071fc69a119
muhammadibrahim-dev <muhammadibrahimmughal35@gmail.com>
2024-12-08 03:09:35 -0800

    for of uses in array

---
+++ index.js
@@ -0,0 +1,9 @@
+const array = ['0', '13', '14','15','16'];
+
+for (const element of array ) {
+  console.log(element);
+}
+
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
