b1c11a36d39d9884e1742651e096da97c7e8db8a
muhammadibrahim-dev <muhammadibrahimmughal35@gmail.com>
2024-12-09 03:45:35 -0800

    class and constructor

--- constructors/index.js
+++ constructors/index.js
@@ -1,12 +1,3 @@
-// const array = ['0', '13', '14','15','16'];
-
-// for (const element of array ) {
-//   console.log(element);
-// }
-
-// // Expected output: "a"
-// // Expected output: "b"
-// // Expected output: "c"
 
 
 // use Class and constructor
